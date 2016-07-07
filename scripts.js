cb315f0057c16fae18f3bc6c3734269380b9b6b8
MiddyGoesDev <finnrietz@googlemail.com>
2016-07-07 13:13:52 +0200

    Index + Scripts + Css + Video.mp4

---
+++ scripts.js
@@ -0,0 +1,45 @@
+function playVid() {
+  var vid = document.getElementById("thelab");
+    vid.play();
+}
+
+function pauseVid() {
+  var vid = document.getElementById("thelab");
+    vid.pause();
+}
+
+
+function playsection1()
+{
+  var vid = document.getElementById("thelab");
+  vid.currentTime=16;
+  vid.play();
+}
+
+function playsection2()
+{
+  var vid = document.getElementById("thelab");
+  vid.currentTime=41;
+  vid.play();
+}
+
+function playsection3()
+{
+  var vid = document.getElementById("thelab");
+  vid.currentTime=3;
+  vid.play();
+}
+
+function playsection4()
+{
+  var vid = document.getElementById("thelab");
+  vid.currentTime=173;
+  vid.play();
+}
+
+function playsection5()
+{
+  var vid = document.getElementById("thelab");
+  vid.currentTime=95;
+  vid.play();
+}
